aa5ccd63fbf3a4792723eab1ab9272da06cda28c
Ricardo Pramana Suranta <ricardo.pramana@gmail.com>
2015-04-29 13:59:52 +0700

    Add project files

---
+++ Example/Pods/Target Support Files/Pods-RPSViewControllerPresenter-RPSViewControllerPresenter/Pods-RPSViewControllerPresenter-RPSViewControllerPresenter-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RPSViewControllerPresenter_RPSViewControllerPresenter : NSObject
+@end
+@implementation PodsDummy_Pods_RPSViewControllerPresenter_RPSViewControllerPresenter
+@end
